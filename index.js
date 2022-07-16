57e963ffcb518059dd0f2a82d5c8096bdfbcd6f9
HFelixH <felix-hoang@outlook.com>
2022-07-16 16:20:36 +0930

    commit tree 2356f89c852bbb82d175d6cd95adfdf3ac596695

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+
+console.log(shout("Hello!"))
+//==========================
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+console.log(whisper("HELLO!"))
+
